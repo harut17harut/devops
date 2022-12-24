197006fec98febd938f4407cdc43ddd425382623
harut <harut15harut@gmail.com>
2022-12-24 11:22:56 +0400

    yaml file created

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ const express = require('express')
 const app = express()
 
 app.listen(3000,() =>{
-    console.log('Server lissening to port 3000');
+    console.log('Server listens port 3000');
 })
