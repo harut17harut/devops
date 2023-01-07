db145479caab9fbe575f0559eb587b387712f98e
harut <harut15harut@gmail.com>
2023-01-07 22:10:34 +0400

    fsfs

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  cloud {
+    organization = "harut15harut"
+
+    workspaces {
+      name = "test"
+    }
+  }
+}
+
+
+provider "aws" {
+  region = "eu-central-1"
+  required_version = ">= 1.2.0"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-0039da1f3917fa8e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform instance"
+  }
+}
